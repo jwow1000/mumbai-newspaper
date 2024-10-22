944e8f216ca034a38d49886bd11d3691ba04da7a
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-10-22 15:50:01 -0400

    better grid placement of items

--- main.js
+++ main.js
@@ -13,19 +13,21 @@ function onScroll( event ) {
   const hori = event.horizontal;
   
   if( vert === "down") {
-    console.log("dowwwn")
-    loopX += -0.1;
+    loopX += 5;
+    if( loopX < 0 ) {
+      loopX = 100;
+    } 
   } else if( vert === "up") {
-    console.log("upppp")
-    loopX += 0.1;
+    loopX -= 5;
+    if ( loopX > 100) {
+      loopX = 0;
+    }
   }
 
   if( hori === "left") {
-    console.log("lefft")
     loopY += -0.1;
   } else if( hori === "right") {
     loopY += 0.1;
-    console.log("righhht", loopY)
   }
   render();
   // console.log("handleScroll", event)
@@ -49,24 +51,36 @@ function render() {
     if(!start) {
       item.style.opacity = 1;
     }
-    const yMod = Math.floor( idx / 10 );
-    const yPos = (yMod / 10) * 10;
-    const zDepth = yMod / 10;
-    const xWidth = (yMod / 10) * 300;
-    // go from -20% to +20%
-    const yWidth = (Math.cos( (yMod / 10) * (Math.PI)) * 40) - 20;
 
-    const sine = Math.sin(((idx / total) + loopY) * (Math.PI * 2) );
-    const cos = Math.cos(((idx / total) + loopY + loopX) *  (Math.PI * 2 ) );
-    const normSine = (sine / 2) + 0.5;
-    const normCos = (cos / 2) + 0.5;
+    // calculate yPositon based on groups of 10, normalize
+
+    // width circle math, rotate around yAxis
+    const yAxisControl = (idx % 10) / 10;
+    console.log("yAxis control: ", yAxisControl)
+    const sineY = Math.sin( (yAxisControl + loopY) * (Math.PI * 2) );
+    const cosY = Math.cos( (yAxisControl + loopY) * (Math.PI * 2 ) );
+    const normSineY = (sineY / 2) + 0.5;
+    const normCosY = (cosY / 2) + 0.5;
+
+    // get og yPos with divide and floor
+    let getYPos = Math.floor( idx / 10 ) * 10;
+
+    const yPos = ( loopX + getYPos ) % 100;
+    console.log("wtfff", yPos)
+
     
-    // x translate absolute
-    item.style.left = `${(normSine * 300) - 120}%`;
-    item.style.top = `${(normCos * yWidth) + yPos}%`; 
+
+    // x and y translate 
+    // translate pseudo Z axis, using scale
+    item.style.transform = `
+      translate( 
+        ${((normSineY * 400) - 200) + 100}%,
+        ${((yPos/100) * 1000) - 200}%
+      )
+      scale(${( normCosY ) / 2})
+    `;
     
-    // translate Z axis 
-    item.style.transform = `scale(${normCos + 0.3})`;
+    
   });
 
 }
